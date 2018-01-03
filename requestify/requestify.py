6a1f8f32a8348ff6d4a019bd3632f3ac4e48b4c3
Guillermo Gabbarín <g.gabarrin@gmail.com>
2018-01-02 21:10:16 -0300

    Read raw HTTP request from file

--- requestify/requestify.py
+++ requestify/requestify.py
@@ -1,3 +1,4 @@
+import sys
 from jinja2 import Environment, PackageLoader
 
 from http_request import HTTPRequest
@@ -31,19 +32,13 @@ def generate_request_code(request, selected_template):
 
 
 def main():
-    raw_http_request = (
-        "GET / HTTP/1.1\r\n"
-        "Host: localhost:8080\r\n"
-        "User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/57.0\r\n"
-        "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n"
-        "Accept-Language: en-US,en;q=0.5\r\n"
-        "Accept-Encoding: gzip, deflate\r\n"
-        "DNT: 1\r\n"
-        "Connection: keep-alive\r\n"
-        "Upgrade-Insecure-Requests: 1\r\n"
-    )
+    # Read and parse raw HTTP request
+    file_path = sys.argv[1]
+    raw_http_request = "".join(open(file_path, "r").readlines())
+    print raw_http_request
     request = HTTPRequest(raw_http_request)
     if request:
+        # Generate source code that makes request
         generated_code = generate_request_code(request, PYTHON_REQUESTS_TEMPLATE)
         print generated_code
 
