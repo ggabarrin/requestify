d2a0e61869a208b16baff0ceedd76f84ba4ad0c5
Guillermo Gabarrín <g.gabarrin@gmail.com>
2018-04-06 09:21:36 -0300

    Add tests

--- requestify/tests/test_http_request.py
+++ requestify/tests/test_http_request.py
@@ -1,18 +1,16 @@
 import unittest
 
-import pytest
-
-from http_request import HTTPRequest
+from requestify.http_request import HTTPRequest
 
 
 class HTTPRequestTest(unittest.TestCase):
 
     def setUp(self):
-        raw_http_request = "".join(open("../examples/post.txt", "r").readlines())
+        raw_http_request = "".join(open("requestify/examples/post.txt", "r").readlines())
         self.http_request = HTTPRequest(raw_http_request)
 
     def test_headers(self):
-        assert self.headers == {
+        assert self.http_request.headers == {
             'content-length': '44',
             'accept-language': 'en-US,en;q=0.5',
             'accept-encoding': 'gzip, deflate',
@@ -21,21 +19,22 @@ class HTTPRequestTest(unittest.TestCase):
             'user-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/57.0',
             'dnt': '1',
             'connection': 'keep-alive',
+            'cookie': 'yummy_cookie=choco; tasty_cookie=strawberry',
             'upgrade-insecure-requests': '1',
             'content-type': 'application/x-www-form-urlencoded',
         }
 
     def test_data(self):
-        assert self.data == "user=the_user&password=super_secure_password"
+        assert self.http_request.data == "user=the_user&password=super_secure_password"
 
     def test_cookies(self):
-        assert self.cookies == {
+        assert self.http_request.cookies == {
             'yummy_cookie': 'choco',
             'tasty_cookie': 'strawberry',
         }
 
     def test_method(self):
-        assert self.command == "POST"
+        assert self.http_request.command == "POST"
 
     def test_path(self):
-        assert self.path == "login"
+        assert self.http_request.path == "/login"
